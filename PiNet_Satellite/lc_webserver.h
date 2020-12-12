91fce1ec378467280477f84721346c53dbcae616
raimund89 <raimund89@users.noreply.github.com>
2020-12-13 00:28:14 +0100

    Information page

--- PiNet_Satellite/lc_webserver.h
+++ PiNet_Satellite/lc_webserver.h
@@ -9,22 +9,62 @@
 #include "lc_gpio.h"
 #include "lc_templates.h"
 
+#define SSDP_URL String("/")
+#define SSDP_SCHEMA_URL "description.xml"
+#define SSDP_HTTP_PORT 80
+#define SSDP_NAME "PiNet Sattelite"
+#define SSDP_SERIAL_NUMBER ESP.getChipId()
+#define SSDP_MODEL_NAME "RGB"
+#define SSDP_MODEL_NUMBER "01"
+#define SSDP_MANUFACTURER "PiNet"
+
 ESP8266WebServer server(80);
 
 void InitWebserver()
 {
   server.on("/", []() {
-    server.send(200, "text/html", FrontMatter() + HeadMatter() + ButtonMatter("/description.xml", "SSDP Info") + BackMatter());
-    // User wants some device info:
-    /*DynamicJsonDocument doc(200);
-    doc["ip"] = WiFi.localIP().toString();
-    doc["mac"] = WiFi.macAddress();
-    doc["hostname"] = WiFi.hostname();
-    doc["wifistatus"] = WiFi.status();
-    String output;
-    serializeJson(doc, output);
+    server.send(200, "text/html", FrontMatter() + HeadMatter() + ButtonMatter("/info", "Information") + BackMatter());
+  });
+  server.on("/info", []() {
+    String str = FrontMatter() + HeadMatter();
     
-    server.send(200, "text/json", output);*/
+    str += InfoHeader("General");
+    str += InfoRow("Manufacturer", "Espressiff");
+    str += InfoRow("Device Type", "RGB");
+    str += InfoRow("Friendly Name", "Fractal Lamp");
+    str += InfoRow("PiNet version", "0.1");
+
+    str += InfoHeader("Chip Info");
+    long mils = millis();
+    int mins = int((mils/(1000*60)) % 60);
+    int hrs = int((mils/(1000*60*60)) % 24);
+    int dys = int((mils/(1000*60*60*24)) % 365);
+    str += InfoRow("Uptime", String(dys) + "d " + hrs + "h " + mins + "m");
+    //str += InfoRow("Flash writes", "");
+    //str += InfoRow("Boot count", "");
+    // Flash size
+    // Program size
+    // Free program space
+    // Free memory
+    
+
+    str += InfoHeader("WiFi Info");
+    str += InfoRow("SSID", WiFi.SSID());
+    str += InfoRow("Network IP", WiFi.localIP().toString());
+    str += InfoRow("MAC-address", WiFi.macAddress());
+    
+    str += InfoHeader("SSDP Discovery");
+    str += InfoRow("Schema URL", SSDP_URL + SSDP_SCHEMA_URL);
+    str += InfoRow("Port", String(SSDP_HTTP_PORT));
+    str += InfoRow("Name", SSDP_NAME);
+    str += InfoRow("Serial Number", String(SSDP_SERIAL_NUMBER));
+    str += InfoRow("Manufacturer", SSDP_MANUFACTURER);
+    str += InfoRow("Model Name", SSDP_MODEL_NAME);
+    str += InfoRow("Model Number", SSDP_MODEL_NUMBER);
+    
+    str += BackMatter();
+
+    server.send(200, "text/html", str);
   });
   server.on("/cm", []() {
     if(server.hasArg("cmnd")) {
@@ -69,14 +109,14 @@ void InitWebserver()
   });
   server.begin();
 
-  SSDP.setSchemaURL("description.xml");
-  SSDP.setHTTPPort(80);
-  SSDP.setName("PiNet Sattelite");
-  SSDP.setSerialNumber(ESP.getChipId());
-  SSDP.setURL("index.html");
-  SSDP.setModelName("RGB");
-  SSDP.setModelNumber("01");
-  SSDP.setManufacturer("PiNet");
+  SSDP.setSchemaURL(SSDP_URL + SSDP_SCHEMA_URL);
+  SSDP.setHTTPPort(SSDP_HTTP_PORT);
+  SSDP.setName(SSDP_NAME);
+  SSDP.setSerialNumber(SSDP_SERIAL_NUMBER);
+  SSDP.setURL(SSDP_URL);
+  SSDP.setModelName(SSDP_MODEL_NAME);
+  SSDP.setModelNumber(SSDP_MODEL_NUMBER);
+  SSDP.setManufacturer(SSDP_MANUFACTURER);
   SSDP.begin();
   
   Program_Clear();
